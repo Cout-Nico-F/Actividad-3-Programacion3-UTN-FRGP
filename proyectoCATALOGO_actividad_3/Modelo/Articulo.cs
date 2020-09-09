67f36b8df6dd8294d3ceb7d9dc4020d66d9d42f8
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-09-08 21:34:04 -0300

    Creando el proyecto Modelo y la clase Articulo
        // por ahora creo los atributos privados porque no tengo claro cuales van a necesitar tener getter y/o setters todavia.
        // despues mientras vallamos necesitando escribirlos o leerlos les hacemos las props
        //Dejo aca tambien la linea que nos va a permitir que ande en las 2 pc ya que tenemos el mismo nombre de base de datos (sera parte del Negocio)
        // conexion.ConnectionString = "data source =localhost\\SQLEXPRESS01; initial catalog =CATALOGO_DB; integrated security =sspi";

---
+++ proyectoCATALOGO_actividad_3/Modelo/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+  class Articulo
+  {
+    private int id;
+    private string codigo;
+    private string descripcion;
+    private string marca;
+    private string categoria;
+    // por ahora creo los atributos privados porque no tengo claro cuales van a necesitar tener getter y/o setters todavia.
+    // despues mientras vallamos necesitando escribirlos o leerlos les hacemos las props
+    //Dejo aca tambien la linea que nos va a permitir que ande en las 2 pc ya que tenemos el mismo nombre de base de datos (sera parte del Negocio)
+    // conexion.ConnectionString = "data source =localhost\\SQLEXPRESS01; initial catalog =CATALOGO_DB; integrated security =sspi";
+  }
+}
