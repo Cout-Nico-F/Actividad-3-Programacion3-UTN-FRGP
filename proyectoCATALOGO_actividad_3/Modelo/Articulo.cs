d872d9cd86bf6d433c07cea67bbc91dc0098f8ce
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-09-14 13:51:58 -0300

    Identacion +  Link con informacion sobre casteo de MoneySQL type
    https://www.google.com/search?sxsrf=ALeKk002bgju6EHG1kmBPsT_JAkJaPRX3g%3A1600101530256&ei=mpxfX--TD5Cz5OUPyp2Z8AM&q=c%23+how+to+recieve+money+SQL+type&oq=c%23+how+to+recieve+money+SQL+type&gs_lcp=CgZwc3ktYWIQAzIECCEQCjoECAAQRzoHCCMQsAIQJzoGCAAQCBAeUOyqAli0xgJgptQCaAFwBXgAgAFtiAG_DZIBBDE1LjSYAQCgAQGqAQdnd3Mtd2l6yAEIwAEB&sclient=psy-ab&ved=0ahUKEwjvyMLaiunrAhWQGbkGHcpOBj4Q4dUDCA0&uact=5

--- proyectoCATALOGO_actividad_3/Modelo/Articulo.cs
+++ proyectoCATALOGO_actividad_3/Modelo/Articulo.cs
@@ -9,17 +9,18 @@ namespace Modelo
   public class Articulo
   {
     private int id;
-        //private string codigo;
-        public string codigo { get; set; }
-        //private string descripcion;
-        public string descripcion { get; set; }
-        public string nombre { get; set; }
+    //private string codigo;
+    public string codigo { get; set; }
+    //private string descripcion;
 
-        public string imagenUrl { get; set; }
+    public string nombre { get; set; }
+    public string descripcion { get; set; }
+        
+    public string imagenUrl { get; set; }
 
-        public Decimal precio { get; set; }
+    public Decimal precio { get; set; }
 
-        private string marca;
+    private string marca;
     private string categoria;
     // por ahora creo los atributos privados porque no tengo claro cuales van a necesitar tener getter y/o setters todavia.
     // despues mientras vallamos necesitando escribirlos o leerlos les hacemos las props
