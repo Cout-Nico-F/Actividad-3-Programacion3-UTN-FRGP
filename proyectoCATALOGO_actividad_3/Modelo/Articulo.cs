94ac9170719766824a543e3098f01a24b6f2f1f4
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-09-14 15:20:20 -0300

    Agregando clases para  composicion de Marca y Categoria.

--- proyectoCATALOGO_actividad_3/Modelo/Articulo.cs
+++ proyectoCATALOGO_actividad_3/Modelo/Articulo.cs
@@ -12,16 +12,18 @@ namespace Modelo
     //private string codigo;
     public string codigo { get; set; }
     //private string descripcion;
-
     public string nombre { get; set; }
+
     public string descripcion { get; set; }
         
     public string imagenUrl { get; set; }
 
     public Decimal precio { get; set; }
 
-    private string marca;
-    private string categoria;
+    public Marca Marca { get; set; }
+
+    public Categoria Categoria { get; set; }
+
     // por ahora creo los atributos privados porque no tengo claro cuales van a necesitar tener getter y/o setters todavia.
     // despues mientras vallamos necesitando escribirlos o leerlos les hacemos las props
     //Dejo aca tambien la linea que nos va a permitir que ande en las 2 pc ya que tenemos el mismo nombre de base de datos (sera parte del Negocio)
