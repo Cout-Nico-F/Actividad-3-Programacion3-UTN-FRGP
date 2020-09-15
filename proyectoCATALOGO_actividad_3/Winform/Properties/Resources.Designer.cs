b39435ed6e426cc826966196f1fb01f4eb5b2bf9
AlonsoHS21 <alonsohua20@gmail.com>
2020-09-14 21:18:30 -0300

    Cambio imagenes
    Cambie las imagenes de los botones de Aceptar y Cancelar de el formAlta

--- proyectoCATALOGO_actividad_3/Winform/Properties/Resources.Designer.cs
+++ proyectoCATALOGO_actividad_3/Winform/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace Winform.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Imagen_Aceptar {
+            get {
+                object obj = ResourceManager.GetObject("Imagen_Aceptar", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Imagen_Agregar {
             get {
                 object obj = ResourceManager.GetObject("Imagen_Agregar", resourceCulture);
