3fed0b20c54fb74a459b0b2c7ca93a1a9eca76f1
Cout-Nico-F <n.ferreiraa@live.com.ar>
2020-09-18 21:07:39 -0300

    intentando arreglar el problema del URL

--- proyectoCATALOGO_actividad_3/Negocios/ArticulosNegocio.cs
+++ proyectoCATALOGO_actividad_3/Negocios/ArticulosNegocio.cs
@@ -81,7 +81,7 @@ namespace Negocios
 
             connection.ConnectionString = "data source =localhost\\SQLEXPRESS01; initial catalog =CATALOGO_DB; integrated security =sspi";
             command.CommandType = System.Data.CommandType.Text;
-            command.CommandText = "insert into ARTICULOS(Codigo,Nombre,Descripcion,Precio,IdMarca,IdCategoria,ImagenUrl) values ('" + nuevo.codigo + "','" + nuevo.nombre + "','" + nuevo.descripcion + "','" + nuevo.precio + "','"+ m.Id +"','"+ c.Id + "','" + nuevo.imagenUrl + "')"; ;
+            command.CommandText = "insert into ARTICULOS(Codigo,Nombre,Descripcion,Precio,IdMarca,IdCategoria,ImagenUrl) values ('" + nuevo.codigo + "','" + nuevo.nombre + "','" + nuevo.descripcion + "','" + nuevo.precio + "','"+ m.Id +"','"+ c.Id + "','"+ nuevo.imagenUrl +"')";
             command.Connection = connection;
             connection.Open();
             command.ExecuteNonQuery();
