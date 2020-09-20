303204871e2e44c111cf3e89b56d14eae2076633
AlonsoHS21 <alonsohua20@gmail.com>
2020-09-20 19:00:20 -0300

    Lo que no se guardo

--- proyectoCATALOGO_actividad_3/Winform/Form_Message.Designer.cs
+++ proyectoCATALOGO_actividad_3/Winform/Form_Message.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form_Message
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form_Message";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form_Message";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mensaje";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
